ef40b9644ecfe06ef418c271782a6911d0b7dd9b
AMRIBOUTHEINA <boutheina_amri@yahoo.com>
2021-04-29 02:03:06 +0200

    mise a jour

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { facebookloginbutton } from "react-social-login-buttons";
+import { FacebookLoginButton } from "react-social-login-buttons";
+import { InstagramLoginButton } from "react-social-login-buttons";
+import {TwitterLoginButton } from "react-social-login-buttons";
   function App() {
   return (
-    <Form className="App">
+    <div className="App">
+       <body className="App-body">
       <h1><span className="font-weight-bold">mywebsite</span>.com</h1>
       <h2>Hello</h2>
 
@@ -24,9 +27,14 @@ import { facebookloginbutton } from "react-social-login-buttons";
    <Button className="btn-lg btn-dark btn-block">Log in
    </Button> 
    <div className="text-center pt-3">or continue with your social acount</div>
-   <Button className="BTN1">FOFGET MY Password</Button>
-   <Button className="BTN2">LOGIN UP</Button>
-      </Form>
+   <FacebookLoginButton className="faceb" />
+   <InstagramLoginButton className="insta"/>
+   <TwitterLoginButton className="tweet"/>
+  
+   <Button className="BTN1">FOFGET MY Password</Button><br/><Button  className="BTN2">LOGIN UP</Button>
+   
+   </body>
+      </div>
   );
 }
 
